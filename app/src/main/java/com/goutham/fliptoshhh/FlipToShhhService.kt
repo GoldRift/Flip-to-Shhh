949dfcbb0b6a43f3217d369e3c70cbbddee5273e
Goutham Krishna Shabu <gouthamshabu@gmail.com>
2024-10-02 22:36:06 -0600

    Tweaks and comments

--- app/src/main/java/com/goutham/fliptoshhh/FlipToShhhService.kt
+++ app/src/main/java/com/goutham/fliptoshhh/FlipToShhhService.kt
@@ -4,26 +4,25 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.media.AudioManager
 import android.os.Build
 import android.os.IBinder
-import android.os.VibrationEffect
 import android.os.Vibrator
 import android.util.Log
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 
 
 class FlipToShhhService : Service(), SensorEventListener {
 
-    private lateinit var coordsofxyz: TextView
     private lateinit var sensorManager: SensorManager
     private lateinit var vibrator: Vibrator
+    private lateinit var audioManager: AudioManager
+    private var faceDown = false
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -32,27 +31,31 @@ class FlipToShhhService : Service(), SensorEventListener {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d("FlipToShhhService", "Service started")
 
+        // Creates notification channel
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel("flip_to_shhh", "Flip to Shhh", NotificationManager.IMPORTANCE_DEFAULT)
             val notificationManager = getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(channel)
         }
 
+        // Builds the notification for running in the background
         val notificationIntent = Intent(this, R::class.java)
         val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
         val notification = android.app.Notification.Builder(this, "flip_to_shhh")
             .setContentTitle("Flip to Shhh")
-            .setContentText("Service is running")
+            .setContentText("App is running")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentIntent(pendingIntent)
             .build()
         startForeground(1, notification)
 
-
+        // Gets the system sensor service
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
+        // Gets the system vibrator service
+        vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
 
+        // Sets the sensor manager attributes
         sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
             sensorManager.registerListener(
                 this,
@@ -64,15 +67,7 @@ class FlipToShhhService : Service(), SensorEventListener {
         return super.onStartCommand(intent, flags, startId)
     }
 
-    override fun onDestroy() {
-        Log.d("FlipToShhhService", "Service stopped")
-        sensorManager.unregisterListener(this)
-        stopForeground(true)
-        super.onDestroy()
-    }
-
-
-
+    // Function to check the sensor orientation and changes the faceDown variable
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onSensorChanged(event: SensorEvent?) {
 
@@ -81,15 +76,21 @@ class FlipToShhhService : Service(), SensorEventListener {
             val y = event.values[1]
             val z = event.values[2]
 
-
-
-            if (x < 1 && y < 1 && z < -9) {
-                vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE))
-            }
-
+            faceDown = x < 1 && y < 1 && z < -9
         }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
+
+    // If the service is fully dead it executes these
+    override fun onDestroy() {
+        Log.d("FlipToShhhService", "Service stopped")
+
+        // Stops the sensor service
+        sensorManager.unregisterListener(this)
+        stopForeground(true)
+        super.onDestroy()
+    }
+
 }
