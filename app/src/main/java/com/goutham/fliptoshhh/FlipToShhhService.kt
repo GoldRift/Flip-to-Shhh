3a54029683c022e364e98869157894968b765e94
Goutham Krishna Shabu <gouthamshabu@gmail.com>
2024-11-08 01:04:34 -0700

    Fixes ringer mode not returning to original

--- app/src/main/java/com/goutham/fliptoshhh/FlipToShhhService.kt
+++ app/src/main/java/com/goutham/fliptoshhh/FlipToShhhService.kt
@@ -21,8 +21,10 @@ class FlipToShhhService : Service(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private lateinit var audioManager: AudioManager
-    private var faceDown = false
+    private lateinit var notificationManager: NotificationManager
     private val deBounceTime = 2000
+    private var originalRingerMode: Int? = null
+    private var faceDown = false
     private var lastFlipTime: Long = 0
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -37,6 +39,9 @@ class FlipToShhhService : Service(), SensorEventListener {
 
         // Gets the system sensor service
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Gets the notification manager service
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         // Creates notification channel
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -82,18 +87,30 @@ class FlipToShhhService : Service(), SensorEventListener {
             val isFaceDown = x < 2 && x > -2 && y < 2 && y > -2 && z < -9 // Checks the face down status
             val currentTime = System.currentTimeMillis() // Gets time for debounce time
 
+            // Check is DND is active
+            val isDndActive = notificationManager.currentInterruptionFilter != NotificationManager.INTERRUPTION_FILTER_ALL
+
 
             if (isFaceDown && !faceDown && currentTime - lastFlipTime > deBounceTime) {
                 faceDown = true
                 lastFlipTime = currentTime
-                audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT)
-                Log.i("FlipToShhhService", "Phone flipped face down. Ringer mode set to SILENT.")
+
+                if (!isDndActive && audioManager.ringerMode != AudioManager.RINGER_MODE_SILENT &&
+                    audioManager.ringerMode != AudioManager.RINGER_MODE_VIBRATE) {
+                    originalRingerMode = audioManager.ringerMode // Store the current mode before changing it
+                    audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT)
+                    Log.i("FlipToShhhService", "Phone flipped face down. Ringer mode set to SILENT.")
+                }
             } else if (!isFaceDown && faceDown && currentTime - lastFlipTime > deBounceTime) {
                 faceDown = false
                 lastFlipTime = currentTime
-                if (audioManager.ringerMode != AudioManager.RINGER_MODE_NORMAL) {
-                    audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL)
-                    Log.i("FlipToShhhService", "Phone flipped upright. Ringer mode set to NORMAL.")
+
+                if (originalRingerMode != null && audioManager.ringerMode == AudioManager.RINGER_MODE_SILENT) {
+                    if (!isDndActive) { // Only restore if DND is not active
+                        audioManager.setRingerMode(originalRingerMode!!)
+                        Log.i("FlipToShhhService", "Phone flipped upright. Ringer mode restored to original state: $originalRingerMode")
+                        originalRingerMode = null // Reset after restoring
+                    }
                 }
             }
         }
